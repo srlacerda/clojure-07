c4155f29271af8ff7e2a3e636a4712d8e916e9d8
Diego Lacerda <diego.lacerda.alves@gmail.com>
2022-04-25 20:02:11 -0300

    defspec e prop/for-all

--- hospital/test/hospital/logic_test.clj
+++ hospital/test/hospital/logic_test.clj
@@ -3,7 +3,9 @@
   (:require [clojure.test :refer :all]
             [hospital.logic :refer :all]
             [hospital.model :as h.model]
+            [clojure.test.check.clojure-test :refer (defspec)]
             [clojure.test.check.generators :as gen]
+            [clojure.test.check.properties :as prop]
             [schema.core :as s]))
 
 (s/set-fn-validation! true)
@@ -14,8 +16,11 @@
   (testing "Que cabe numa vila fazia"
     (is (cabe-na-fila? {:espera []} :espera)))
 
+  ; o doseq com um simbolo e uma sequencia gerada funciona
+  ; mas.... talvez não seja o que queremos em example-based manual, vamos
+  ; ver um problema de dois simbolos
   (testing "Que cabe pessoas em filas de tamanhao ate 4 inclusive"
-    (doseq [fila (gen/sample (gen/vector gen/string-alphanumeric 0 4) 1000)]
+    (doseq [fila (gen/sample (gen/vector gen/string-alphanumeric 0 4) 100)]
       (is (cabe-na-fila? {:espera fila}, :espera))))
 
 
@@ -34,7 +39,35 @@
   (testing "Que não cabe quando o departamento não existe"
     (is (not (cabe-na-fila? {:espera [1 2 3 4]}, :raio-x)))))
 
+; aqui tivemos um problema
+; o doseq na unha gera uma multiplicacao de casos
+; incluindo muuuuuuuitos casos repetidos
+; que nao tem nada ligado com o que queremos
+;(deftest chega-em-test
+;  (testing "Que é colocada uma pessoa em filas menores que 5"
+;    (doseq [fila (gen/sample (gen/vector gen/string-alphanumeric 0 4) 10)
+;            pessoa (gen/sample gen/string-alphanumeric 5)]
+;      (println pessoa fila)
+;      (is (= 1 1)) ; só para mostrarq que são 50 asserts (10 * 5)
+;      )
+;    ))
 
+; muito importante lembrar que se voce esta rodando um repl continuo e recarregando
+; os testes. você corre o risco de uma função que foi definida antigamente continuar
+; carregada no seu namespace e continuar rodando ela. nesse caso lembrar de restart o repl
+
+; o teste a seguir é generativo e funciona
+; mas... o resultado dele parece MUITO uma cópia do nosso código implementado
+; {:espera (conj fila pessoa)} == o codigo que eu escrevi la
+; se eu coloquei um bug lá, provavelmente eu coloquei um bug aqui, e ai vai dar true e o bug continua.
+; nao importa se escreve o teste ANTES ou DEPOS... é o mesmo codigo. (não é 100%)
+; mas a medida que ficasse mais complexo o teste.
+(defspec coloca-uma-pessoa-em-filas-menores-que-5 100
+         (prop/for-all
+           [fila (gen/vector gen/string-alphanumeric 0 4)
+            pessoa gen/string-alphanumeric]
+           (is (= {:espera (conj fila pessoa)}
+                  (chega-em {:espera fila} :espera pessoa)))))
 
 
 
